18acec82089a3aeb54a2a8ee955fb9d9e0754ff9
khero <m.laroui@esi-sba.dz>
2024-08-27 11:32:51 +0100

    pre_prod

--- hubspot_apis/settings.py
+++ hubspot_apis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-9=eac7ehbxf17%d_a0-+5mh-+xn+wib64ocm+7a5$gkftd5v2s'
+
 
 
 load_dotenv()
